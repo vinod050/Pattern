1255d43e35defea3d6d1499839aed13cd485416c
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 16:56:04 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+public class pattern {
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+        // for (int i = 5; i >=1; i-- ){
+
+            // for(int j=1; j<=i;j++){
+            //     System.out.print(j);
+            // }
+            for(int j=1; j<=5-i+1;j++){
+                System.out.print(j);
+            }
+            // for(int j=1;j<=i;j++){
+            //     System.out.print(i);
+            // }
+            System.out.println(" ");
+        }
+    }
+}
